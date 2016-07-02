04a9ba33cfec1e48f0b028992d20d1b64414ead2
sillychip <mechanic.tanvir@gmail.com>
2016-07-02 08:56:53 +0000

    Update uva_978.cpp
    edited for optimization

--- uva_978.cpp
+++ uva_978.cpp
@@ -9,54 +9,60 @@
     {
     int power;
     long b,sb,sg;
-    multiset<int,greater<int> > blue_lemmings,green_lemmings;
+    vector<int> blue_lemmings,green_lemmings;
+    vector<int>::iterator it;
     scanf("%ld %ld %ld",&b,&sg,&sb);
     
     while(sg--)
     {
     scanf("%d",&power);
-    green_lemmings.insert(power);
+    green_lemmings.push_back(power);
     }
-    
+    sort(green_lemmings.begin(),green_lemmings.end());
     
     while(sb--)
     {
     scanf("%d",&power);
-    blue_lemmings.insert(power);
+    blue_lemmings.push_back(power);
     }
-    
-    multiset<int>blue_surv,green_surv;
-    
+    sort(blue_lemmings.begin(),blue_lemmings.end());
+     
     while(!blue_lemmings.empty() && !green_lemmings.empty())
     {
-
-    for(int j=0;j<b;j++)
+    vector<int> blue_surv,green_surv;
+	int j=b;
+    while(j--)
     {
+    if(blue_lemmings.back() > green_lemmings.back()) blue_surv.push_back(blue_lemmings.back() - green_lemmings.back());
+    else if(blue_lemmings.back() < green_lemmings.back()) green_surv.push_back(green_lemmings.back()-blue_lemmings.back());
+    blue_lemmings.pop_back();
+    green_lemmings.pop_back();
     if(blue_lemmings.empty() || green_lemmings.empty()) break;
-    if(*blue_lemmings.begin() > *green_lemmings.begin()) blue_surv.insert((*blue_lemmings.begin()) - (*green_lemmings.begin()));
-    else if(*blue_lemmings.begin() < *green_lemmings.begin()) green_surv.insert((*green_lemmings.begin())-(*blue_lemmings.begin()));
-    blue_lemmings.erase(blue_lemmings.begin());
-    green_lemmings.erase(green_lemmings.begin());
+    }
+    sort(blue_surv.begin(),blue_surv.end());
+    sort(green_surv.begin(),green_surv.end());
+    it=blue_lemmings.end();
+    blue_lemmings.insert(it,blue_surv.begin(),blue_surv.end());
+    inplace_merge(blue_lemmings.begin(),it,blue_lemmings.end());
+    it=green_lemmings.end();
+    green_lemmings.insert(it,green_surv.begin(),green_surv.end());
+    inplace_merge(green_lemmings.begin(),it,green_lemmings.end());
+    
+    
     }
     
-    blue_lemmings.insert(blue_surv.begin(),blue_surv.end());
-    green_lemmings.insert(green_surv.begin(),green_surv.end());
-    blue_surv.clear();
-    green_surv.clear();
-    }
-    multiset<int>::iterator it;
     if(!blue_lemmings.empty())
     {
     printf("blue wins\n");
-    it=blue_lemmings.begin();
-    while(it !=blue_lemmings.end()) printf("%d\n",*it++);
+    vector<int>::reverse_iterator rit=blue_lemmings.rbegin();
+    while(rit != blue_lemmings.rend()) printf("%d\n",*rit++);
     }
     
     else if(!green_lemmings.empty())
     {
     printf("green wins\n");
-    it=green_lemmings.begin();
-    while(it != green_lemmings.end()) printf("%d\n",*it++);
+    vector<int>::reverse_iterator rit=green_lemmings.rbegin();
+    while(rit != green_lemmings.rend()) printf("%d\n",*rit++);
     }
     else printf("green and blue died\n");
     if(tc)printf("\n");
